0a1de7b420bae67ef8fd699aef3df544de8249e1
Jasa-Hollow <joel2262000@gmail.com>
2022-10-05 19:49:14 -0600

    Diseño login

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+<!-- Hereda la plantilla definida -->
+@extends('layouts.login') @extends('layouts.headerlogin')
+
+@section('header')
+
+@endsection
+
+<body>
+
+@section('conten_login')
+
+@endsection
+
+</body>
--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,20 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* ------------------------------------------------------------------------ */
+/* | Rutas del sistema | */
+/* Login */
+Route::get('/', function () { return view('/index'); });
+Route::get('/index', function () { return view('/index'); });
 
-Route::get('/', function () {
-    return view('welcome');
-});
+/* Vista - Inicio */
+Route::get('/home', function () { return view('user/home'); });
+/* Vista - Administrador */
+Route::get('/admin', function () { return view('user/admin'); });
+/* Vista - Productos */
+Route::get('/product', function () { return view('user/product'); });
+/* Vista - Ventas */
+Route::get('/sold', function () { return view('user/sold'); });
+/* Vista - Inventario */
+Route::get('/stock', function () { return view('user/stock'); });
+/* ------------------------------------------------------------------------ */
